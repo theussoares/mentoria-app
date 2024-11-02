d1b857489c534109bc736d14e7e1b534b985965a
theussoares <matheusfs774@gmail.com>
2024-11-02 12:25:15 -0400

    fix: lanf hml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'pt-BR' // ou o idioma desejado, como 'en' para inglês
+      }
+    }
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
