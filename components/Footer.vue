bece8efcdcf52f532798eae43d420785ee3f3b61
theussoares <matheusfs774@gmail.com>
2024-11-02 12:19:59 -0400

    fix: fooer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,24 @@
 <template>
-  <footer class="bg-[#0E050D] w-full h-12 text-[#F9F8F6] flex flex-col justify-center items-center p-10">
-    <a href="https://instagram.com/theuus_soares" rel="noopener noreferrer" target="_blank" aria-label="Visitar perfil no Instagram de theuus_soares">Instagram: theuus_soares</a>
-    <a href="https://github.com/theussoares" rel="noopener noreferrer" target="_blank" aria-label="Visitar repositório no Github de theussoares">Github: theussoares</a>
+  <footer
+    class="bg-[#0E050D] w-full h-12 text-[#F9F8F6] flex flex-col justify-center items-center p-10"
+  >
+    <a
+      href="https://instagram.com/theuus_soares"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      Instagram: theuus_soares
+    </a>
+    <a
+      href="https://github.com/theussoares"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      Github: theussoares
+    </a>
   </footer>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
